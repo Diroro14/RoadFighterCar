91b17231e920ea3cf5376f3800de9ceb85c3c4dd
Diroro14 <91137829+Diroro14@users.noreply.github.com>
2021-09-21 15:24:15 +0100

    Add files via upload

---
+++ practica1/src/es/ucm/tp1/logic/Game.java
@@ -0,0 +1,30 @@
+package es.ucm.tp1.logic;
+
+import es.ucm.tp1.control.Level;;
+
+public class Game {
+
+	long gameSeed;
+	Level gameLevel;
+	Player player;
+	
+	public Game (Long seed, Level level) { 
+		gameSeed = seed;
+		gameLevel = level;
+	}
+	
+	private static void update() {
+		
+		
+	}
+	
+	public static void toggleTest() {
+		
+	}
+	
+	public static String getGameStatus() {
+		
+		return "";
+	}
+	
+}
---
+++ practica1/src/es/ucm/tp1/logic/Player.java
@@ -0,0 +1,15 @@
+package es.ucm.tp1.logic;
+
+public class Player {
+
+	private static int x;
+	private static int y;
+	private static int behaviour;
+	private static int speed = 1;
+	private static int resistsance;
+	
+	private static void update() {
+		
+	}
+	
+}
